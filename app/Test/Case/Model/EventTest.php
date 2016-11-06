a9520daafd40848c06d4872ce4817fbdbdda3853
ferretdayo <ferretdayo@hotmail.co.jp>
2016-11-06 19:08:03 +0900

    テストを動くようにした　CIはまだ

--- app/Test/Case/Model/EventTest.php
+++ app/Test/Case/Model/EventTest.php
@@ -1,12 +1,14 @@
 <?php
 App::uses('Event', 'Model');
 class EventTest extends CakeTestCase {
-	
+	// EventFixtureのデータの取得
+	public $fixtures = array('app.event');
 	private $Event = null;
-	
+
 	//SetUp
 	public function setUp() {
-		$this->Event = new Event;
+		// EventFixtureのテストデータをModelに挿入
+		$this->Event = ClassRegistry::init('Event');
 		parent::setUp();
 	}
 	//TearDown
@@ -14,12 +16,24 @@ class EventTest extends CakeTestCase {
 		unset($this->Event);
 		parent::tearDown();
 	}
-	
+
 	// テストケース dayDiff(x, y)
 	function testDayDiff() {
 		$_SESSION['event_id'] = 1;
 		$result = $this->Event->dayDiff();
-		$this->assertEquals(1, $result);
+		$this->assertEquals(2, $result);
+	}
+
+	//DBの内容が予想とあっているかどうか(idとイベント名のみ照合。必要に応じて変更すべきかな)
+	public function testPublished(){
+		$result = $this->Event->find('all', array('fields' => array('id', 'event_name')));
+		$expected = array(
+			array('Event' => array('id' => 1, 'event_name' => 'First Event')),
+			array('Event' => array('id' => 2, 'event_name' => 'Second Article')),
+			array('Event' => array('id' => 3, 'event_name' => 'Third Article')),
+		);
+
+		$this->assertEquals($expected, $result);
 	}
 }
 ?>
