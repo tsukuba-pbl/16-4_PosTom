38605b19cc5682a664c4be734f44ca05b30f2b48
ferretdayo <ferretdayo@hotmail.co.jp>
2016-11-09 17:35:47 +0900

    Fixture作った

--- app/Test/Case/Controller/EventsControllerTest.php
+++ app/Test/Case/Controller/EventsControllerTest.php
@@ -2,7 +2,23 @@
 App::uses('AppController', 'Controller');
 
 class EventsControllerTest extends ControllerTestCase {
-	public $fixtures = array('app.event');
+	public $fixtures = array('app.user', 'app.editor', 'app.event');
+
+	public function setUp(){
+		parent::setUp();
+		$testUser = array(
+			'User' => array(
+				'username' => 'ferret',
+				'password' => '12345678'
+			)
+		);
+		$this->testAction('/users/login', array('data' => $testUser, 'method' => 'post'));
+	}
+
+	public function tearDown(){
+		$this->testAction('/users/logout');
+		parent::tearDown();
+	}
 
 	// viewテスト
 	public function testView(){
@@ -32,5 +48,27 @@ class EventsControllerTest extends ControllerTestCase {
 		$this->assertEquals($expected, $this->vars);
 	}
 
+	// addEventのテスト
+	public function testAddEvent(){
+		$expected = array(
+			'Event' => array(
+				'event_name' => 'testEvent1',
+				'event_location' => 'Japan',
+				'event_begin_date' => '2015-01-01',
+				'event_begin_time' => '12:00:00',
+				'event_end_date' => '2015-01-02',
+				'event_end_time' => '12:00:00',
+				'event_vote_app' => 0,
+				'event_vote_valid' => 0,
+			)
+		);
+		$result = $this->testAction(
+			'/events/add',
+			array(
+				'data' => $expected, 'method' => 'post'
+			)
+		);
+		var_dump($result);
+	}
 }
 ?>
