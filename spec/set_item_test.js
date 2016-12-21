892986d4a7854ded0530c3066be49c219b9bf963
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2016-12-21 20:07:10 +0900

    テストコード修正です

--- spec/set_item_test.js
+++ spec/set_item_test.js
@@ -3,27 +3,10 @@ describe("set_item> ", function(){
 		beforeEach(function(){
 			spyOn(console, "log");
 		    var data = {};
-				var voteinfo = {"voter_name": "テスト", "voter_id": "test"};
+				var voteinfo = {"voter_name": "テスト", "voter_id": "test", "voter_type": "1"};
 				var str_voteinfo = JSON.stringify(voteinfo);
 				localStorage.clear();
-
-			$('body').append("<div id='voterid'></div>");
-			$('body').append("<input type='hidden' name='checkvote' id='checkvote' value='0'>");
-			$("#voterid").val(str_voteinfo);
-
 		})
-
-		//QRコードが入力されていない場合のテストコード。
-		it("non-dataInQRcode", function() {
-			var selected_json = {
-				contender1:"111",
-				contender2:"222",
-				contender3:"333"
-			};
-			localStorage.setItem('Candidate_ID', JSON.stringify(selected_json));
-			set_item();
-			expect(console.log).toHaveBeenCalledWith("data = null");
-		});
 
 		//候補者が3人未満の場合のテストコード。
 		it("under_3", function(){
