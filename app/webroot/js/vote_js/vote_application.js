ec027d4f95ebb10ae155b9dbb7259d102ff770de
YRZhong <zhongyuran@gmail.com>
2017-01-18 19:41:41 +0900

    conflictsfix3

--- app/webroot/js/vote_js/vote_application.js
+++ app/webroot/js/vote_js/vote_application.js
@@ -1,19 +1,4 @@
-
 function vote_application() {
-  $.fn.goToVotePage = function(ev) {
-
-	$(this).on(ev, function() {
-		if (voter_param_flag === 0) {
-			changePage("#votePage");
-		}
-		else if (voter_param_flag === 1) {
-			alert ("配布されたQRコードをもう一度読み込んで下さい。");
-		}
-  }
-}
-
-
-
   if (already_voted == 1) {
   $.mobile.changePage("#AlreadyVotedPage", {
         changeHash: true
