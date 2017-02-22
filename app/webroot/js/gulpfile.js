774e14cca15fc09716b41fc6daaa0a5694d705a1
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2017-02-22 16:55:57 +0900

    ファイルの圧縮

--- app/webroot/js/gulpfile.js
+++ app/webroot/js/gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require('gulp-rename');
 
 gulp.task('js', function() {
-    gulp.src('./vote_js/*.js')
+    return gulp.src('./vote_js/*.js')
         .pipe(concat('vote_app.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./vote_js'));
     console.log('JSを結合しました。');
 });
